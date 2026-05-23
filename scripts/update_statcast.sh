ef14a36c6a5a5e7e249396bd6732c622d2901ac6
Jeremy Piech <jypiech@gmail.com>
2026-05-23 09:31:37 -0600

    Update copy to last 2 weeks

--- scripts/update_statcast.sh
+++ scripts/update_statcast.sh
@@ -2,7 +2,7 @@
 
 source .env
 
-for i in $(seq 1 10); do
+for i in $(seq 1 14); do
   date=$(date -v-${i}d +%Y-%m-%d)
   year=$(echo "$date" | cut -d'-' -f1)
   aws s3 cp s3://${STATCAST_S3_BUCKET}/${year}/statcast-${date}.csv ${DATA_DIRECTORY}/statcast/${year}/statcast-${date}.csv
