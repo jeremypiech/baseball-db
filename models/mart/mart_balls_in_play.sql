121b1b6d5d2eeb825962a4630e69bcc696cdc4fa
Jeremy Piech <jypiech@gmail.com>
2026-05-29 21:31:38 -0600

    Revert "Add fact_statcast_pitches and mart_balls_in_play"
    This reverts commit 029468eb63d274df7f7afb60fe6e27986341ec27.

--- models/mart/mart_balls_in_play.sql
+++
@@ -1,8 +0,0 @@
-WITH fact_statcast_pitches AS (
-    SELECT * FROM {{ ref('fact_statcast_pitches') }}
-)
-
-SELECT *
-FROM fact_statcast_pitches
-WHERE pitch_result_desc = 'hit_into_play'
-    AND pa_event != 'home_run'
